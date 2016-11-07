3be0fd62cf570857fedcee814c30ed686212d4e9
wildskyf <poppin.wildsky+gh@gmail.com>
2016-11-08 00:33:48 +0800

    init commit

---
+++ init.el
@@ -0,0 +1,89 @@
+;; package.el 相關設定
+(require 'package)
+
+(setq package-archives '(
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")))
+(package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load-theme 'monokai t)
+
+(global-unset-key (kbd "C-z"))
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;
+; some minor setting
+;;;;;;;;;;;;;;;;;;;;;;;
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+
+
+
+
+
+
+
+;;;;;;;;;;;;
+; web-mode
+;;;;;;;;;;;;;;;
+
+;(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(defun my-web-mode-hook ()
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+
+
+;;;;;;;;;;
+; smex
+;;;;;;;;;;;;
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+
+
+
+
+
+
+
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smex web-mode colour-region column-enforce-mode column-marker rainbow-mode org))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
